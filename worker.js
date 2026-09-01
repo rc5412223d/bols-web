7ed041ad2cc75dd3b672c046f302c10adcb7abdb
rc5412223d <chenneww6666@gmail.com>
2026-09-02 01:00:29 +0800

    Implement ChatRoom class and fetch method

--- worker.js
+++ worker.js
@@ -1,1 +1,26 @@
+export class ChatRoom {
+  constructor(state, env) {
+    this.state = state;
+    this.env = env;
+  }
 
+  async fetch(request) {
+    return new Response("ChatRoom OK");
+  }
+}
+
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+
+    // 測試 Durable Object
+    if (url.pathname === "/chat-test") {
+      const id = env.CHAT_ROOM.idFromName("main");
+      const room = env.CHAT_ROOM.get(id);
+
+      return room.fetch(request);
+    }
+
+    return new Response("無垠伺服器登入與聊天服務正常運作。");
+  }
+};
